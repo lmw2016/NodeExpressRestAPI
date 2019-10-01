7fbf614f245b1caf04def48c10ab243b8d56db82
liming wu <limwuly@gmail.com>
2019-10-01 13:13:46 -0400

    implement filter with query string and getting single item by id

--- app.js
+++ app.js
@@ -9,7 +9,24 @@ const Book=require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req,res)=>{
-      Book.find((err,books)=>{
+     //const {query}=req;
+     const query={};
+     if(req.query.genre){
+       query.genre=req.query.genre;
+     }
+      Book.find(query,(err,books)=>{
+        if(err){
+          return   res.send(err);
+        }
+        return  res.json(books);
+        
+      });
+  })
+
+  bookRouter.route('/books/:bookId')
+  .get((req,res)=>{
+    
+      Book.findById(req.params.bookId,(err,books)=>{
         if(err){
           return   res.send(err);
         }
