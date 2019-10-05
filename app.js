a776b43fff2cf9e6edd0937fd75b09bf8456cd93
liming wu <limwuly@gmail.com>
2019-10-05 13:35:07 -0400

    implement POST to mongodb bookApi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const bodyParser=require('body-parser')
 const app=express();
 const db=mongoose.connect('mongodb://localhost/bookApi',{useNewUrlParser: true,useUnifiedTopology: true}); //must be bookApi to match db name case
 const bookRouter=express.Router();
@@ -7,7 +8,16 @@ const bookRouter=express.Router();
 const port=process.env.PORT||3000;
 const Book=require('./models/bookModel');
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 bookRouter.route('/books')
+.post((req,res)=>{
+  const book=new Book(req.body);
+  book.save();
+  console.log(book);
+  return res.status(201).json(book);
+})
   .get((req,res)=>{
      //const {query}=req;
      const query={};
@@ -24,6 +34,7 @@ bookRouter.route('/books')
   })
 
   bookRouter.route('/books/:bookId')
+  
   .get((req,res)=>{
     
       Book.findById(req.params.bookId,(err,books)=>{
